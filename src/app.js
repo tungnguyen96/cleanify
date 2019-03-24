6981aa428c2e7df386e2d2ca3e2dd7e211c8e5b6
hungp96 <quanghwng22@gmail.com>
2019-03-24 21:00:32 +0700

    Add database usage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
+
+mongoose.connect('mongodb://localhost/test_db', { useNewUrlParser: true });
+const Dummy = mongoose.model('Dummy', { title: String, body: String });
+
+app.get('/dummies/create', (req, res) => {
+    const dummy = new Dummy({ title: 'first dummy', body: 'oh yeah~~~'});
+    dummy.save().then(() => {
+        res.send('dummy saved!');
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
