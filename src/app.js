b81aff726036f457698f821d2d140e72d235ef05
hungpq96 <quanghwng22@gmail.com>
2019-03-25 17:25:22 +0700

    Config test suite

--- src/app.js
+++ src/app.js
@@ -2,16 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000;
 
 mongoose.connect('mongodb://localhost/test_db', { useNewUrlParser: true });
 const Dummy = mongoose.model('Dummy', { title: String, body: String });
 
 app.get('/dummies/create', (req, res) => {
-    const dummy = new Dummy({ title: 'first dummy', body: 'oh yeah~~~'});
-    dummy.save().then(() => {
-        res.send('dummy saved!');
-    })
-})
+  const dummy = new Dummy({ title: 'first dummy', body: 'oh yeah~~~' });
+  dummy.save().then(() => {
+    res.send('dummy saved!');
+  });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports = app;
